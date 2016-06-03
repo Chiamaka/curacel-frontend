853bd63b008020e9c1301bcc900c2e1fd61768a6
Chiamaka Nwolisa <chiamakanwolisa@gmail.com>
2016-06-03 11:49:04 +0000

    Registration for a Hospital

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -1,14 +1,33 @@
-import Ember from 'ember';
-// import ENV from '../server/config.js';
+import Ember from 'ember'; 
 
-export default Ember.Controller.extend({
-	actions:{
-		clickRegister: function(){
-			var hospitalName = this.get('hospitalName');
-			var hospitalAddress = this.get('hospitalAddress');
-			var adminName = this.get('adminName');
-			var email = this.get('email');
-			var password = this.get('password');
-		}
-	}
+var RegisterController = Ember.Controller.extend({
+  session: Ember.inject.service(),
+  errorMessage: null,
+  email: null,
+  password: null,
+
+  actions: {
+    register() {
+    	var hospitalName = this.get('hospitalName');
+		var hospitalAddress = this.get('hospitalAddress');
+		var adminName = this.get('adminName');
+		//var email = this.get('email');
+      	let { email, password } = this.getProperties('email', 'password');
+		let userData = {
+			hospitalName,
+			hospitalAddress,
+			adminName
+		};
+
+      	this.get('session').authenticate('authenticator:register', {
+        	email: email,
+        	password: password,
+        	userData
+     	}).catch((error) => {
+        	this.set('errorMessage', error.reason);
+      	});
+    }
+  }
 });
+
+export default RegisterController;
